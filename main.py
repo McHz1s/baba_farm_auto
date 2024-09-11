9286b6ca27dd77e8f93555ecc361dff12c286924
ly z <316793523@qq.com>
2024-09-11 23:35:45 +0800

    Make subprocess to bypass apium server side bug

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
+import argparse
+import multiprocessing
+
 from driver.taobao import Taobao
+from driver.alipay import Alipay
 from utils.appium_utils import retry_n
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='config')
+    parser.add_argument('--app',
+                        nargs='?',
+                        type=str,
+                        default='Taobao',
+                        help='Configuration file to use', )
+    return parser.parse_args()
+
 def main():
+    args = parse_args()
     seed = {
-        'keyword': 'Taobao'
+        'keyword': args.app,
     }
-    spider = Taobao()
+    spider = globals()[args.app]()
     while True:
         spider.execute(seed)
 
 
 if __name__ == '__main__':
-    main()
+    exit_code = 1
+    while exit_code != 0:
+        process = multiprocessing.Process(target=main)
+        process.start()
+        process.join()
+        exit_code = process.exitcode
+        print(f"Process ended with exit code: {exit_code}")
